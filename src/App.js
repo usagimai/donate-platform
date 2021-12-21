5b57923fd886c6d1f2701cf3eb8c26d36120e12d
Kotake <82296998+usagimai@users.noreply.github.com>
2021-12-21 19:58:32 +0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Routes, Route } from "react-router";
+
 import "./app.scss";
 import Nav from "./components/nav/Nav";
 import Home from "./pages/Home";
+import ItemPage from "./pages/ItemPage";
 import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <Nav />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/item" element={<ItemPage />} />
+      </Routes>
       <Footer />
     </>
   );
